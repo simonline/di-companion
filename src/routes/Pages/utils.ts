151f7ac6a669a5f37b210d67cda478ae5f07dba5
Simon Liedtke <simon.liedtke@gmail.com>
2024-08-13 22:07:08 +0200

    fix tsc

--- src/routes/Pages/utils.ts
+++ src/routes/Pages/utils.ts
@@ -3,7 +3,8 @@ import type { Theme } from '@mui/material';
 function getPageHeight(theme: Theme) {
   const topSpacing = Number(theme.mixins.toolbar.minHeight) + parseInt(theme.spacing(1));
   const bottomSpacing = parseInt(
-    theme.components?.MuiBottomNavigation?.styleOverrides?.root?.minHeight,
+    // @ts-expect-error minHeight is not in the type definition
+    theme.components?.MuiBottomNavigation?.styleOverrides?.root?.minHeight || '0',
   );
   return `calc(100vh - ${topSpacing}px - ${bottomSpacing}px)`;
 }
