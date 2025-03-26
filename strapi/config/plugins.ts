3d6be260d4b5aeb5b5c941cf278900ad5e51e83e
root <root@rti.wechange.de>
2025-03-26 18:24:41 +0000

    live changes

--- strapi/config/plugins.ts
+++ strapi/config/plugins.ts
@@ -2,7 +2,11 @@ export default ({ env }: { env: (key: string, defaultValue?: any) => any }) => (
   "users-permissions": {
     config: {
       register: {
+<<<<<<< Updated upstream
         allowedFields: ["givenName", "familyName", "gender", "position", "bio", "linkedinProfile", "avatar"],
+=======
+        allowedFields: ["givenName", "familyName", "gender", "position", "bio", "linkedinProfile", "phone", "isPhonePublic", "avatar"],
+>>>>>>> Stashed changes
       },
     },
   },
--- strapi/src/api/request/routes/request.ts
+++ strapi/src/api/request/routes/request.ts
@@ -4,4 +4,8 @@
 
 import { factories } from '@strapi/strapi';
 
+<<<<<<< Updated upstream
 export default factories.createCoreRouter('api::request.request'); 
+=======
+export default factories.createCoreRouter('api::request.request');
+>>>>>>> Stashed changes
